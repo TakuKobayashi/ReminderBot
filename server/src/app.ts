3f234cb6682e177e396bab8eca3c6e264c3eafb7
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2020-02-24 18:20:39 +0900

    TwitterのOAuthはpassportを使わない

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,27 +8,12 @@ import { twitterRouter } from './api/routes/twitter/account';
 import { lineBotRouter } from './api/routes/line/bot';
 import { lineNotifyRouter } from './api/routes/line/notify';
 
-const passport = require('passport');
-const TwitterStrategy = require('passport-twitter').Strategy;
-
 const app = express();
 const server = awsServerlessExpress.createServer(app);
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 app.use(cookieParser());
-
-app.use(passport.initialize());
-
-passport.use(new TwitterStrategy({
-    consumerKey: process.env.TWITTER_CONSUMER_KEY,
-    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-    callbackURL: '/twitter/callback',
-  },
-  function(token, tokenSecret, profile, done) {
-    done(undefined, profile);
-  }
-));
 
 app.use(cors({ origin: true }));
 
